44130be384e413f5ba7f1eb3f97a4edc4c7ba3aa
Anton Zuev <anton.zuev@autentia.com>
2019-06-12 15:41:35 +0200

    Initial Commit

---
+++ SampleSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SampleSwiftUI
+//
+//  Created by Anton Zuev on 12/06/2019.
+//  Copyright © 2019 Anton Zuev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
